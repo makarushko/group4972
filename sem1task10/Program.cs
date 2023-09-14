3c35604fb9b9bd656ece0d13ec2f5a17bbeddb78
Anna <makarushko7702@gmail.com>
2023-09-14 15:13:16 +0300

    completed task 10

---
+++ sem1task10/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+   и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Enter a 3-digit number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000) {
+	int result = (number / 10) % 10;
+	Console.WriteLine($"The second digit is {result}");
+} else {
+	Console.WriteLine("Not a 3-digit number. Please try again");
+}

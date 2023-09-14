a7bfb273496214c2ce50ceeca231b87d14659f68
Anna <makarushko7702@gmail.com>
2023-09-14 15:21:52 +0300

    completed task 13

---
+++ sem1task13/Program.cs
@@ -0,0 +1,20 @@
+﻿/* №13 Напишите программу, которая выводит третью цифру
+   заданного числа или сообщает, что третьей цифры нет.
+   
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = number.ToString().Length;
+if (length >= 3) {
+	while (number > 999)
+	{
+		number = number / 10;
+	}
+	int result = number % 10;
+	Console.WriteLine("The third digit is " + result);
+} else {
+	Console.WriteLine("The number contains less than 3 digits");
+}

983f794120d034f32d1f81479e82600169c1988d
Anna <makarushko7702@gmail.com>
2023-09-21 18:25:06 +0300

    completed task 29

---
+++ sem1task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// №29 Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите начальное значение диапазона: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное значение диапазона: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] randomArray = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    randomArray[i] = new Random().Next(minValue,maxValue);
+    Console.Write(randomArray[i] + " ");
+}
